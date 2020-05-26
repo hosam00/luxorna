9d3073bb81c5a1cfef19d3e75242644e1725dd74
samah <samahezz96@gmail.com>
2020-05-26 12:46:34 +0200

    Some Services

---
+++ luxorna/luxorna/ITI.Luxorna.Services/ResturantItemSize/ResturantItemSizeService.cs
@@ -0,0 +1,56 @@
+﻿using ITI.Luxorna.Entities;
+using ITI.Luxorna.Repositories;
+using ITI.Luxorna.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Luxorna.Services
+{
+    public class ResturantItemSizeService
+    {
+        public UnitOfWork UnitOfWork { get; set; }
+        private GenericRepository<ResturantItemSize> ResturantItemSizeRepo;
+        public ResturantItemSizeService(UnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+            ResturantItemSizeRepo = unitOfWork.ResturantItemSizeRepo;
+        }
+        public ResturantItemSizeEditViewModel Add(ResturantItemSizeEditViewModel _ResturantItemSize)
+        {
+            ResturantItemSize ResturantItemSize = ResturantItemSizeRepo.Add(_ResturantItemSize.toModel());
+            UnitOfWork.commit();
+            return ResturantItemSize.toEditViewModel();
+        }
+        public ResturantItemSizeEditViewModel Update(ResturantItemSizeEditViewModel _ResturantItemSize)
+        {
+            ResturantItemSize ResturantItemSize = ResturantItemSizeRepo.Add(_ResturantItemSize.toModel());
+            UnitOfWork.commit();
+            return ResturantItemSize.toEditViewModel();
+        }
+        public ResturantItemSizeViewModel GetByID(int id)
+        {
+            ResturantItemSize _ResturantItemSize = ResturantItemSizeRepo.GetByID(id);
+            return _ResturantItemSize.toViewModel();
+        }
+
+        public IEnumerable<ResturantItemSizeViewModel> GetAll()
+        {
+
+            return ResturantItemSizeRepo.GetAll().ToList().Select(i => i.toViewModel());
+        }
+        public IEnumerable<ResturantItemSizeViewModel> GetFilter(int id)
+        {
+            var ResturantItemSizes = ResturantItemSizeRepo.GetFilter(a => a.ID== id);
+            return ResturantItemSizes.ToList().Select(i => i.toViewModel());
+        }
+
+        public void Remove(ResturantItemSizeEditViewModel resturantItemSize)
+        {
+            ResturantItemSizeRepo.Remove(resturantItemSize.toModel());
+            UnitOfWork.commit();
+        }
+    }
+}
